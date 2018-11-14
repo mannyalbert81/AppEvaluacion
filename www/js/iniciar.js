ab6afe96617c66b8c07010ae5d5545caeedf0b71
Usuario <Usuario@192.168.1.129>
2018-11-14 15:23:41 -0500

    gfdfdg

--- www/js/iniciar.js
+++ www/js/iniciar.js
@@ -22,7 +22,235 @@ function checkConnection() {
    
 	     if (online=='1'){
 	    	 
-	    	 procesos();
+	    	
+	    	 var base_url = 'http://192.168.1.129:4000/webcapremci/webservices_evaluacion/';
+	    		var pag_service = 'SincronizacionService.php?jsoncallback=?' ;
+	    		var nombre_usuarios = "";
+	    		
+	    		$(document).on('click', '#btn_excelente', function(){
+	     		
+	   			 var cedula = $("#cedula").val();
+	   			 var calificacion = "Excelente";
+	   			 var imei= "1111111111";
+	   	    	 var error="";
+
+	   	    	   if (cedula == "")
+				    	{
+					    	
+				    		$("#mensaje_cedula").text("Introduzca Cedula");
+				    		$("#mensaje_cedula").fadeIn("slow"); //Muestra mensaje de error
+				    		error="SI";
+				            return false;
+					    }
+				    	else 
+				    	{
+				    		$("#mensaje_cedula").fadeOut("slow"); //Muestra mensaje de error
+				    		error="NO";
+						}
+	   	    	   
+	   	    	   if(error=="NO"){
+				  		
+	   	    		
+	   	    		 
+		   	    		   $.ajax({
+		   	    			   type: 'GET',
+		   	    			   url: base_url+pag_service,
+		   	    			   data:{cedula:cedula, calificacion:calificacion, imei:imei},
+		   	    			   dataType: 'json',
+		   	    			   success: function (x) {
+		   	    				 
+		   	    				$.each(x, function(i, j) {			
+									nombre_usuarios  =  j.nombre_usuarios;
+								});
+		   	    				swal("Gracias por Calificarnos", "Hola "+nombre_usuarios+" tu calificación se registro correctamente.", "success");	
+		   	    				$("#cedula").val("");
+		   	    			 },
+		   	    				     error: function (jqXHR, textStatus, errorThrown) {
+		   	    				    	swal("Gracias por Calificarnos", "Cedula no existe registrada en nuestra base de datos.", "error");
+		   	    				    	$("#cedula").val("");
+		   	    			 }
+		
+		   	    			});
+	   	    		   
+	   	    		  
+	   	    		   
+				  }
+	    	    	   
+	    		});
+	    		
+	    		
+	    		
+	    		
+	    		
+	    		
+	    		$(document).on('click', '#btn_bueno', function(){
+	         		
+	      			 var cedula = $("#cedula").val();
+	      			 var calificacion = "Bueno";
+	      			 var imei= "1111111111";
+	      	    	 var error="";
+
+	      	    	   if (cedula == "")
+	   			    	{
+	   				    	
+	   			    		$("#mensaje_cedula").text("Introduzca Cedula");
+	   			    		$("#mensaje_cedula").fadeIn("slow"); //Muestra mensaje de error
+	   			    		error="SI";
+	   			            return false;
+	   				    }
+	   			    	else 
+	   			    	{
+	   			    		$("#mensaje_cedula").fadeOut("slow"); //Muestra mensaje de error
+	   			    		error="NO";
+	   					}
+	      	    	   
+	      	    	   if(error=="NO"){
+	      	    		
+	      	    		   $.ajax({
+	      	    			   type: 'GET',
+	      	    			   url: base_url+pag_service,
+	      	    			   data:{cedula:cedula, calificacion:calificacion, imei:imei},
+	      	    			   dataType: 'json',
+	      	    			   success: function (x) {
+	      	    				 
+	      	    				$.each(x, function(i, j) {			
+	   							nombre_usuarios  =  j.nombre_usuarios;
+	   						});
+	      	    					
+	      	    				swal("Gracias por Calificarnos", "Hola "+nombre_usuarios+" tu calificación se registro correctamente.", "success");	
+	       	    				
+	      	    				$("#cedula").val("");
+	      	    			 },
+	      	    				     error: function (jqXHR, textStatus, errorThrown) {
+	      	    				    	swal("Gracias por Calificarnos", "Cedula no existe registrada en nuestra base de datos.", "error");
+	      	    				    	$("#cedula").val("");
+	      	    			 }
+
+	      	    			});
+	      	    		
+	      	    		   
+	   			     
+	      	    		 
+	      	    	   }
+	       	    	   
+	       		});
+	    		
+	    		
+	    		
+
+	    		$(document).on('click', '#btn_regular', function(){
+	         		
+	      			 var cedula = $("#cedula").val();
+	      			 var calificacion = "Regular";
+	      			 var imei= "1111111111";
+	      	    	 var error="";
+
+	      	    	   if (cedula == "")
+	   			    	{
+	   				    	
+	   			    		$("#mensaje_cedula").text("Introduzca Cedula");
+	   			    		$("#mensaje_cedula").fadeIn("slow"); //Muestra mensaje de error
+	   			    		error="SI";
+	   			            return false;
+	   				    }
+	   			    	else 
+	   			    	{
+	   			    		$("#mensaje_cedula").fadeOut("slow"); //Muestra mensaje de error
+	   			    		error="NO";
+	   					}
+	      	    	   
+	      	    	   if(error=="NO"){
+	      	    		 
+	      	    		   $.ajax({
+	      	    			   type: 'GET',
+	      	    			   url: base_url+pag_service,
+	      	    			   data:{cedula:cedula, calificacion:calificacion, imei:imei},
+	      	    			   dataType: 'json',
+	      	    			   success: function (x) {
+	      	    				 
+	      	    				$.each(x, function(i, j) {			
+	   							nombre_usuarios  =  j.nombre_usuarios;
+	   						});
+	      	    					
+	      	    				swal("Gracias por Calificarnos", "Hola "+nombre_usuarios+" tu calificación se registro correctamente.", "success");	
+	       	    				
+	      	    				$("#cedula").val("");
+	      	    			 },
+	      	    				     error: function (jqXHR, textStatus, errorThrown) {
+	      	    				    	swal("Gracias por Calificarnos", "Cedula no existe registrada en nuestra base de datos.", "error");
+	      	    				    	$("#cedula").val("");
+	      	    			 }
+
+	      	    			});
+	      	    		
+	      	    		
+	      	    		   
+	   			  }
+	       	    	   
+	       		});
+	    		
+	    		
+	    		
+	    		
+
+	    		$(document).on('click', '#btn_malo', function(){
+	         		
+	      			 var cedula = $("#cedula").val();
+	      			 var calificacion = "Malo";
+	      			 var imei= "1111111111";
+	      	    	 var error="";
+
+	      	    	   if (cedula == "")
+	   			    	{
+	   				    	
+	   			    		$("#mensaje_cedula").text("Introduzca Cedula");
+	   			    		$("#mensaje_cedula").fadeIn("slow"); //Muestra mensaje de error
+	   			    		error="SI";
+	   			            return false;
+	   				    }
+	   			    	else 
+	   			    	{
+	   			    		$("#mensaje_cedula").fadeOut("slow"); //Muestra mensaje de error
+	   			    		error="NO";
+	   					}
+	      	    	   
+	      	    	   if(error=="NO"){
+	      	    		 
+	      	    		   $.ajax({
+	      	    			   type: 'GET',
+	      	    			   url: base_url+pag_service,
+	      	    			   data:{cedula:cedula, calificacion:calificacion, imei:imei},
+	      	    			   dataType: 'json',
+	      	    			   success: function (x) {
+	      	    				 
+	      	    				$.each(x, function(i, j) {			
+	   							nombre_usuarios  =  j.nombre_usuarios;
+	   						});
+	      	    					
+	      	    				swal("Gracias por Calificarnos", "Hola "+nombre_usuarios+" tu calificación se registro correctamente.", "success");	
+	       	    				
+	      	    				$("#cedula").val("");
+	      	    			 },
+	      	    				     error: function (jqXHR, textStatus, errorThrown) {
+	      	    				    	swal("Gracias por Calificarnos", "Cedula no existe registrada en nuestra base de datos.", "error");
+	      	    				   $("#cedula").val("");
+	      	    			 }
+
+	      	    			});
+	      	    		
+	      	    		   
+	   			  }
+	       	    	   
+	       		});
+	    		
+	    		
+	    	 
+	    		 $( "#cedula" ).focus(function() {
+	   			  $("#mensaje_cedula").fadeOut("slow");
+	   		     });
+	    		 
+	    	 
+	    	 
 	     }else{
 	    	 
 		    	 swal("Capremci Informa", "Tu dispositivo no tiene internet.", "error");
@@ -36,232 +264,7 @@ function checkConnection() {
 
 function procesos() {
 	
-    	    var base_url = 'http://192.168.1.129:4000/webcapremci/webservices_evaluacion/';
-    		var pag_service = 'SincronizacionService.php?jsoncallback=?' ;
-    		var nombre_usuarios = "";
-    		
-    		$(document).on('click', '#btn_excelente', function(){
-     		
-   			 var cedula = $("#cedula").val();
-   			 var calificacion = "Excelente";
-   			 var imei= "1111111111";
-   	    	 var error="";
-
-   	    	   if (cedula == "")
-			    	{
-				    	
-			    		$("#mensaje_cedula").text("Introduzca Cedula");
-			    		$("#mensaje_cedula").fadeIn("slow"); //Muestra mensaje de error
-			    		error="SI";
-			            return false;
-				    }
-			    	else 
-			    	{
-			    		$("#mensaje_cedula").fadeOut("slow"); //Muestra mensaje de error
-			    		error="NO";
-					}
-   	    	   
-   	    	   if(error=="NO"){
-			  		
-   	    		
-   	    		 
-	   	    		   $.ajax({
-	   	    			   type: 'GET',
-	   	    			   url: base_url+pag_service,
-	   	    			   data:{cedula:cedula, calificacion:calificacion, imei:imei},
-	   	    			   dataType: 'json',
-	   	    			   success: function (x) {
-	   	    				 
-	   	    				$.each(x, function(i, j) {			
-								nombre_usuarios  =  j.nombre_usuarios;
-							});
-	   	    				swal("Gracias por Calificarnos", "Hola "+nombre_usuarios+" tu calificación se registro correctamente.", "success");	
-	   	    				$("#cedula").val("");
-	   	    			 },
-	   	    				     error: function (jqXHR, textStatus, errorThrown) {
-	   	    				    	swal("Gracias por Calificarnos", "Cedula no existe registrada en nuestra base de datos.", "error");
-	   	    				    	$("#cedula").val("");
-	   	    			 }
-	
-	   	    			});
-   	    		   
-   	    		  
-   	    		   
-			  }
-    	    	   
-    		});
-    		
-    		
-    		
-    		
-    		
-    		
-    		$(document).on('click', '#btn_bueno', function(){
-         		
-      			 var cedula = $("#cedula").val();
-      			 var calificacion = "Bueno";
-      			 var imei= "1111111111";
-      	    	 var error="";
-
-      	    	   if (cedula == "")
-   			    	{
-   				    	
-   			    		$("#mensaje_cedula").text("Introduzca Cedula");
-   			    		$("#mensaje_cedula").fadeIn("slow"); //Muestra mensaje de error
-   			    		error="SI";
-   			            return false;
-   				    }
-   			    	else 
-   			    	{
-   			    		$("#mensaje_cedula").fadeOut("slow"); //Muestra mensaje de error
-   			    		error="NO";
-   					}
-      	    	   
-      	    	   if(error=="NO"){
-      	    		
-      	    		   $.ajax({
-      	    			   type: 'GET',
-      	    			   url: base_url+pag_service,
-      	    			   data:{cedula:cedula, calificacion:calificacion, imei:imei},
-      	    			   dataType: 'json',
-      	    			   success: function (x) {
-      	    				 
-      	    				$.each(x, function(i, j) {			
-   							nombre_usuarios  =  j.nombre_usuarios;
-   						});
-      	    					
-      	    				swal("Gracias por Calificarnos", "Hola "+nombre_usuarios+" tu calificación se registro correctamente.", "success");	
-       	    				
-      	    				$("#cedula").val("");
-      	    			 },
-      	    				     error: function (jqXHR, textStatus, errorThrown) {
-      	    				    	swal("Gracias por Calificarnos", "Cedula no existe registrada en nuestra base de datos.", "error");
-      	    				    	$("#cedula").val("");
-      	    			 }
-
-      	    			});
-      	    		
-      	    		   
-   			     
-      	    		 
-      	    	   }
-       	    	   
-       		});
-    		
-    		
-    		
-
-    		$(document).on('click', '#btn_regular', function(){
-         		
-      			 var cedula = $("#cedula").val();
-      			 var calificacion = "Regular";
-      			 var imei= "1111111111";
-      	    	 var error="";
-
-      	    	   if (cedula == "")
-   			    	{
-   				    	
-   			    		$("#mensaje_cedula").text("Introduzca Cedula");
-   			    		$("#mensaje_cedula").fadeIn("slow"); //Muestra mensaje de error
-   			    		error="SI";
-   			            return false;
-   				    }
-   			    	else 
-   			    	{
-   			    		$("#mensaje_cedula").fadeOut("slow"); //Muestra mensaje de error
-   			    		error="NO";
-   					}
-      	    	   
-      	    	   if(error=="NO"){
-      	    		 
-      	    		   $.ajax({
-      	    			   type: 'GET',
-      	    			   url: base_url+pag_service,
-      	    			   data:{cedula:cedula, calificacion:calificacion, imei:imei},
-      	    			   dataType: 'json',
-      	    			   success: function (x) {
-      	    				 
-      	    				$.each(x, function(i, j) {			
-   							nombre_usuarios  =  j.nombre_usuarios;
-   						});
-      	    					
-      	    				swal("Gracias por Calificarnos", "Hola "+nombre_usuarios+" tu calificación se registro correctamente.", "success");	
-       	    				
-      	    				$("#cedula").val("");
-      	    			 },
-      	    				     error: function (jqXHR, textStatus, errorThrown) {
-      	    				    	swal("Gracias por Calificarnos", "Cedula no existe registrada en nuestra base de datos.", "error");
-      	    				    	$("#cedula").val("");
-      	    			 }
-
-      	    			});
-      	    		
-      	    		
-      	    		   
-   			  }
-       	    	   
-       		});
-    		
-    		
-    		
-    		
-
-    		$(document).on('click', '#btn_malo', function(){
-         		
-      			 var cedula = $("#cedula").val();
-      			 var calificacion = "Malo";
-      			 var imei= "1111111111";
-      	    	 var error="";
-
-      	    	   if (cedula == "")
-   			    	{
-   				    	
-   			    		$("#mensaje_cedula").text("Introduzca Cedula");
-   			    		$("#mensaje_cedula").fadeIn("slow"); //Muestra mensaje de error
-   			    		error="SI";
-   			            return false;
-   				    }
-   			    	else 
-   			    	{
-   			    		$("#mensaje_cedula").fadeOut("slow"); //Muestra mensaje de error
-   			    		error="NO";
-   					}
-      	    	   
-      	    	   if(error=="NO"){
-      	    		 
-      	    		   $.ajax({
-      	    			   type: 'GET',
-      	    			   url: base_url+pag_service,
-      	    			   data:{cedula:cedula, calificacion:calificacion, imei:imei},
-      	    			   dataType: 'json',
-      	    			   success: function (x) {
-      	    				 
-      	    				$.each(x, function(i, j) {			
-   							nombre_usuarios  =  j.nombre_usuarios;
-   						});
-      	    					
-      	    				swal("Gracias por Calificarnos", "Hola "+nombre_usuarios+" tu calificación se registro correctamente.", "success");	
-       	    				
-      	    				$("#cedula").val("");
-      	    			 },
-      	    				     error: function (jqXHR, textStatus, errorThrown) {
-      	    				    	swal("Gracias por Calificarnos", "Cedula no existe registrada en nuestra base de datos.", "error");
-      	    				   $("#cedula").val("");
-      	    			 }
-
-      	    			});
-      	    		
-      	    		   
-   			  }
-       	    	   
-       		});
-    		
-    		
-    	 
-    		 $( "#cedula" ).focus(function() {
-   			  $("#mensaje_cedula").fadeOut("slow");
-   		     });
-    		 
+    	   
    
 }
 
